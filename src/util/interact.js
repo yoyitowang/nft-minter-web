d14d79c498a01acafbf12aee1cb1f643981b6a59
yoyitowang <yoyowang0720@gmail.com>
2022-03-27 02:24:23 +0800

    fix dotenv

--- src/util/interact.js
+++ src/util/interact.js
@@ -1,68 +1,10 @@
-import { pinJSONToIPFS } from './pinata.js';
-require('dotenv').config();
+import { pinJSONToIPFS } from "./pinata.js";
+require("dotenv").config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const contractABI = require("../contract-abi.json");
+const contractAddress = "0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE";
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
-
-const contractABI = require('../contract-abi.json')
-const contractAddress = "0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE";
-
-export const mintNFT = async (url, name, description) => {
-  //error handling
-  if (url.trim() == "" || (name.trim() == "" || description.trim() == "")) {
-    return {
-      success: false,
-      status: "❗Please make sure all fields are completed before minting.",
-    }
-  }
-
-  //make metadata
-  const metadata = new Object();
-  metadata.name = name;
-  metadata.image = url;
-  metadata.description = description;
-
-  //make pinata call
-  const pinataResponse = await pinJSONToIPFS(metadata);
-  if (!pinataResponse.success) {
-    return {
-      success: false,
-      status: "😢 Something went wrong while uploading your tokenURI.",
-    }
-  }
-  const tokenURI = pinataResponse.pinataUrl;
-
-  window.contract = await new web3.eth.Contract(contractABI, contractAddress);//loadContract();
-  
-  //set up your Ethereum transaction
-  const transactionParameters = {
-    to: contractAddress, // Required except during contract publications.
-    from: window.ethereum.selectedAddress, // must match user's active address.
-    'data': window.contract.methods.mintNFT(window.ethereum.selectedAddress, tokenURI).encodeABI()//make call to NFT smart contract 
-  };
-
-  //sign the transaction via Metamask
-  try {
-    const txHash = await window.ethereum
-      .request({
-        method: 'eth_sendTransaction',
-        params: [transactionParameters],
-      });
-    return {
-      success: true,
-      status: "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" + txHash
-    }
-  } catch (error) {
-    return {
-      success: false,
-      status: "😥 Something went wrong: " + error.message
-    }
-
-  }
-}
-
-
-
 
 export const connectWallet = async () => {
   if (window.ethereum) {
@@ -90,7 +32,7 @@ export const connectWallet = async () => {
             {" "}
             🦊{" "}
             <a target="_blank" href={`https://metamask.io/download.html`}>
-              You must install Metamask, a virtual eth2 wallet, in your
+              You must install Metamask, a virtual Ethereum wallet, in your
               browser.
             </a>
           </p>
@@ -137,7 +79,63 @@ export const getCurrentWalletConnected = async () => {
             </a>
           </p>
         </span>
-      )
-    }
+      ),
+    };
   }
+};
+
+async function loadContract() {
+  return new web3.eth.Contract(contractABI, contractAddress);
 }
+
+export const mintNFT = async (url, name, description) => {
+  if (url.trim() == "" || name.trim() == "" || description.trim() == "") {
+    return {
+      success: false,
+      status: "❗Please make sure all fields are completed before minting.",
+    };
+  }
+
+  //make metadata
+  const metadata = new Object();
+  metadata.name = name;
+  metadata.image = url;
+  metadata.description = description;
+
+  const pinataResponse = await pinJSONToIPFS(metadata);
+  if (!pinataResponse.success) {
+    return {
+      success: false,
+      status: "😢 Something went wrong while uploading your tokenURI.",
+    };
+  }
+  const tokenURI = pinataResponse.pinataUrl;
+
+  window.contract = await new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: window.ethereum.selectedAddress, // must match user's active address.
+    data: window.contract.methods
+      .mintNFT(window.ethereum.selectedAddress, tokenURI)
+      .encodeABI(),
+  };
+
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      success: true,
+      status:
+        "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" +
+        txHash,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message,
+    };
+  }
+};
--- src/util/pinata.js
+++ src/util/pinata.js
@@ -1,13 +1,10 @@
 require('dotenv').config();
 const key = process.env.REACT_APP_PINATA_KEY;
 const secret = process.env.REACT_APP_PINATA_SECRET;
-
 const axios = require('axios');
-
 export const pinJSONToIPFS = async(JSONBody) => {
     const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
-    //making axios POST request to Pinata ⬇️
-    return axios 
+    return axios
         .post(url, JSONBody, {
             headers: {
                 pinata_api_key: key,
@@ -26,6 +23,6 @@ export const pinJSONToIPFS = async(JSONBody) => {
                 success: false,
                 message: error.message,
             }
-
-    });
+           
+        });
 };
